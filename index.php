c77a5419462d6957db200686f8dafde374fd8b34
SL-MichaelNelo <michael@shocklogic.com>
2022-02-14 14:02:45 -0400

    Initial push

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+get_header();
+
+if(get_field('isolate_webpage')){
+    header('Cross-Origin-Opener-Policy: same-origin');
+    header('Cross-Origin-Embedder-Policy: require-corp');
+}
+
+if (have_posts()) {
+	while (have_posts()) {
+		the_post();
+		the_content(); 
+	}
+} else {
+	echo "No content was found";
+}
+
+get_footer();
